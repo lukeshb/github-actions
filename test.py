137f23f7716c640287559d030527521480f22812
lukesh bundele <lukesh.bundele@civitaslearning.com>
2025-07-05 20:26:48 +0530

    First tig

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
+''' This module is to test the fibonachi list'''
 def fibonacci_list(n):
+    ''' this is actual function which takes a number
+    and run for loop on it to give you fib series'''
     fib = [0, 1]
     for i in range(2, n):
         fib.append(fib[i-1] + fib[i-2])

c4e068c9efd438e48a69149502e337ab2fe1beec
lukesh bundele <lukesh.bundele@civitaslearning.com>
2025-07-06 09:02:51 +0530

    all workflows

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-''' This module is to test the fibonachi list'''
-def fibonacci_list(n):
-    ''' this is actual function which takes a number
-    and run for loop on it to give you fib series'''
-    fib = [0, 1]
-    for i in range(2, n):
-        fib.append(fib[i-1] + fib[i-2])
-    return fib
-
-print(fibonacci_list(10))
+""" this is test code for bandit testing"""
+# adding print statement
+print('testing pylint & bandit')
